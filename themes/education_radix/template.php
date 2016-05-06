72580cbf86d4d498f64b42a35870c17aa5794b24
Genevieve Johnson <genevieve-johnson@uiowa.ed>
2016-05-06 14:44:38 -0500

    remove copyright theme setting

--- themes/education_radix/template.php
+++ themes/education_radix/template.php
@@ -9,13 +9,3 @@ $includes_path = dirname(__FILE__) . '/includes/*.inc';
 foreach (glob($includes_path) as $filename) {
   require_once dirname(__FILE__) . '/includes/' . basename($filename);
 }
-
-/**
- * Implements template_preprocess_page().
- */
-function education_radix_preprocess_page(&$variables) {
-  // Add copyright to theme.
-  if ($copyright = theme_get_setting('copyright')) {
-    $variables['copyright'] = check_markup($copyright['value'], $copyright['format']);
-  }
-}
