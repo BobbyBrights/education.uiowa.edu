5317a41fb84a0d637e9ef96245144fc373cd88fc
Genevieve Johnson <genevieve-johnson@uiowa.ed>
2016-05-06 16:49:12 -0500

    remove unnecessary field markup from field_scholarship_recipients

--- themes/education_radix/templates/field/field--field-scholarship-recipients.tpl.php
+++ themes/education_radix/templates/field/field--field-scholarship-recipients.tpl.php
@@ -44,14 +44,7 @@
  * @ingroup themeable
  */
 ?>
-
-<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php if (!$label_hidden): ?>
-    <h2 class="field-label"<?php print $title_attributes; ?>><?php print $label ?></h2>
-  <?php endif; ?>
-  <div class="field-items"<?php print $content_attributes; ?>>
-    <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
-    <?php endforeach; ?>
-  </div>
-</div>
+<?php if (!$label_hidden): ?>
+  <h2><?php print $label ?></h2>
+<?php endif; ?>
+<?php print render($items); ?>
