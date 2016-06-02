0ca1a3c1c26317768298d84e80b31fa8397f0709
Genevieve Johnson <genevieve-johnson@uiowa.ed>
2016-06-02 09:52:56 -0500

    Revert "Merge branch 'test_deploy'"
    This reverts commit fe12c89207cf4ce726baf708d1be225a374e107a, reversing
    changes made to 36ca827c0351cff946ce6279f6c22142702ec913.

--- themes/education_radix/templates/page/page.tpl.php
+++ themes/education_radix/templates/page/page.tpl.php
@@ -11,11 +11,41 @@
  */
 ?>
 <header id="header" class="header" role="header">
-  <div class="branding">
-    <div class="container">
-      <a href="<?php print $front_page; ?>" class="site-name" rel="home" title="<?php print t('Home'); ?>">Collge of <span class="site-name-bold">Education</span></a>
-    </div>
-  </div>
+  <div class="container">
+    <nav class="navbar navbar-default" role="navigation">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <?php if ($site_name || $logo): ?>
+          <a href="<?php print $front_page; ?>" class="navbar-brand" rel="home" title="<?php print t('Home'); ?>">
+            <?php if ($logo): ?>
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="logo" />
+            <?php endif; ?>
+            <?php if ($site_name): ?>
+              <span class="site-name"><?php print $site_name; ?></span>
+            <?php endif; ?>
+          </a>
+        <?php endif; ?>
+      </div> <!-- /.navbar-header -->
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="navbar-collapse">
+        <?php if ($main_menu): ?>
+          <ul id="main-menu" class="menu nav navbar-nav">
+            <?php print render($main_menu); ?>
+          </ul>
+        <?php endif; ?>
+        <?php if ($search_form): ?>
+          <?php print $search_form; ?>
+        <?php endif; ?>
+      </div><!-- /.navbar-collapse -->
+    </nav><!-- /.navbar -->
+  </div> <!-- /.container -->
 </header>
 
 <div id="main-wrapper">
