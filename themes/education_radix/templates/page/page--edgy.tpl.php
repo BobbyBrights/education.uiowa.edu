719ee682aae98994380c81ea437b33f825f00862
Genevieve Johnson <genevieve-johnson@uiowa.edu>
2016-06-15 12:56:29 -0500

    remove inner container for main content

--- themes/education_radix/templates/page/page--edgy.tpl.php
+++ themes/education_radix/templates/page/page--edgy.tpl.php
@@ -51,45 +51,43 @@
 <div id="main-wrapper">
   <div id="bg-image" aria-hidden=true></div>
   <div id="main" class="main">
-    <div class="container">
-      <h1 class="offscreen title"><?php print $title; ?></h1>
-      <div class="row">
-        <?php if ($sidebar_first): ?>
-          <div id="sidebar-first" class="col-md-<?php print $sidebar_first_width; ?>" >
-            <?php print render($page['sidebar_first']); ?>
+    <h1 class="offscreen title"><?php print $title; ?></h1>
+    <div class="row">
+      <?php if ($sidebar_first): ?>
+        <div id="sidebar-first" class="col-md-<?php print $sidebar_first_width; ?>" >
+          <?php print render($page['sidebar_first']); ?>
+        </div>
+      <?php endif; ?>
+
+      <div id="content" class="col-md-<?php print $content_width; ?>">
+        <?php if ($messages): ?>
+          <div id="messages">
+            <?php print $messages; ?>
           </div>
         <?php endif; ?>
-
-        <div id="content" class="col-md-<?php print $content_width; ?>">
-          <?php if ($messages): ?>
-            <div id="messages">
-              <?php print $messages; ?>
-            </div>
-          <?php endif; ?>
-          <?php if ($breadcrumb): ?>
-            <div id="breadcrumb" class="visible-desktop">
-              <?php print $breadcrumb; ?>
-            </div>
-          <?php endif; ?>
-          <div id="page-header">
-            <?php if ($title): ?>
-              <div class="page-header">
-                <div class="h1 title"><?php print $title; ?></div>
-              </div>
-            <?php endif; ?>
-            <?php if ($tabs): ?>
-              <div class="tabs">
-                <?php print render($tabs); ?>
-              </div>
-            <?php endif; ?>
-            <?php if ($action_links): ?>
-              <ul class="action-links">
-                <?php print render($action_links); ?>
-              </ul>
-            <?php endif; ?>
+        <?php if ($breadcrumb): ?>
+          <div id="breadcrumb" class="visible-desktop">
+            <?php print $breadcrumb; ?>
           </div>
-          <?php print render($page['content']); ?>
+        <?php endif; ?>
+        <div id="page-header">
+          <?php if ($title): ?>
+            <div class="page-header">
+              <div class="h1 title"><?php print $title; ?></div>
+            </div>
+          <?php endif; ?>
+          <?php if ($tabs): ?>
+            <div class="tabs">
+              <?php print render($tabs); ?>
+            </div>
+          <?php endif; ?>
+          <?php if ($action_links): ?>
+            <ul class="action-links">
+              <?php print render($action_links); ?>
+            </ul>
+          <?php endif; ?>
         </div>
+        <?php print render($page['content']); ?>
       </div>
     </div>
   </div> <!-- /#main -->
