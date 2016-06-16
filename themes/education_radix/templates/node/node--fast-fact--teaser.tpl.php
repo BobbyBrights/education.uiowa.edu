af4aa3c4c2fded2de98f54883aa45f01f48dd50e
Genevieve Johnson <genevieve-johnson@uiowa.edu>
2016-06-16 11:25:52 -0500

    style fast facts in a grid

---
+++ themes/education_radix/templates/node/node--fast-fact--teaser.tpl.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Radix theme implementation to display a node.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+</article>
