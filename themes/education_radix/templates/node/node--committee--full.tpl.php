20ca8da76eea40f1f2a9250e0996b5fe330410c7
Hanna, Dawn <dawn-hanna@uiowa.edu>
2016-07-01 09:03:55 -0500

    Committee full node, refeaturized and created page template

---
+++ themes/education_radix/templates/node/node--committee--full.tpl.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file
+ * Radix theme implementation to display a node.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <div class="content row"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+    ?>
+      <div class="leading-text">
+        <div class="container">
+
+
+           <div <?php if ($content['field_committee_leading_buttons']): ?> class="col-sm-8" <?php else: ?> class="col-sm-12" <?php endif; ?>>
+              <?php print render($content['field_committee_leading_text']); ?>
+            </div>
+            <?php if ($content['field_committee_leading_buttons']): ?>
+              <div class="col-sm-4">
+                <?php print render($content['field_committee_leading_buttons']); ?>
+              </div>
+            <?php endif; ?>
+        </div>
+      </div>
+    <div class="content-section">
+      <?php print render($content); ?>
+    </div>
+  </div>
+
+</article>
