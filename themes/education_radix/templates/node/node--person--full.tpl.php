bdc842c29ee606f8da8aa2c30d48cc25d37b5b2c
Hanna, Dawn <dawn-hanna@uiowa.edu>
2016-08-30 13:37:13 -0500

    Person (staff) profiles. Changed full display and its template.

--- themes/education_radix/templates/node/node--person--full.tpl.php
+++ themes/education_radix/templates/node/node--person--full.tpl.php
@@ -19,7 +19,20 @@
     <div id="container" ng-app="aprProfile" class="person_aprProfile">
       <content-item hawk_id="<?php echo render($content['field_person_apr_id'][0]['#markup']); ?>" apr_key="a7sd98f798asydjkbasbdfasdf79798asdfh013" population_id="9" template_id="5"></content-item>
     </div>
-    <?php /*** End code from Manjoine ***/ ?>
+
+    <?php
+    drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular.min.js', array(
+            'type' => 'external',
+            'scope' => 'footer', ));
+    drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js', array(
+            'type' => 'external',
+            'scope' => 'footer', ));
+    drupal_add_js('https://apps.its.uiowa.edu/facadmin/assets/lib/angular-filter/dist/angular-filter.min.js?v=4', array(
+            'type' => 'external',
+            'scope' => 'footer', ));
+    drupal_add_js(drupal_get_path('theme','education_radix') . '/js/apr_profile.js', array('scope' => 'footer'));
+
+    /*** End code from Manjoine ***/ ?>
 
   <?php } else { ?>
 
@@ -34,24 +47,40 @@
           $building = $content['field_person_building'][0]['sitenow_map_row']['content']['#markup'];
           hide($content['field_person_building']);
           $content['field_person_office_number'][0]['#markup'] .= ' ' . $building;
-        }
+        } ?>
 
-        print render($content);
-      ?>
+        <div <?php if ($content['field_person_image']): ?> class="col-sm-2"<?php else: ?> class="col-sm-12"<?php endif; ?> >
+           <?php print render($content['field_person_image']); ?>
+        </div>
+        <div <?php if ($content['field_person_image']): ?> class="col-sm-9"<?php else: ?> class="col-sm-12"<?php endif; ?> >
+         <div class="media-body">
+          <table cellspacing="5">
+            <tbody>
+              <?php if ($content['field_person_working_title']): ?><tr><th class="pull-right">Title </th>
+                        <td style="padding-left:8px;" class="ng-binding"><?php print render($content['field_person_working_title']); ?></td></tr><?php endif; ?>
+              <?php if ($content['field_person_office_number']): ?><tr><th class="pull-right">Office </th>
+                        <td style="padding-left:8px;" class="ng-binding"><?php print render($content['field_person_office_number']); ?></td></tr><?php endif; ?>
+              <?php if ($content['field_person_phone_number']): ?><tr><th class="pull-right">Phone </th>
+                        <td style="padding-left:8px;" class="ng-binding"><?php print render($content['field_person_phone_number']); ?></td></tr><?php endif; ?>
+              <?php if ($content['field_person_email']): ?><tr><th class="pull-right">Email </th>
+                        <td style="padding-left:8px;" class="ng-binding"><?php print render($content['field_person_email']); ?></td></tr><?php endif; ?>
+            </tbody>
+          </table>
+         </div>
+        </div>
+        <br style="clear:both;" />
+        <?php if ($content['field_person_affiliations']): ?>
+          <h3>Affiliations within the College</h3>
+          <?php print render($content['field_person_affiliations']); ?>
+        <?php endif; ?>
+        <?php if ($content['field_person_expertise']): ?><
+          <h3>Areas of Expertise</h3>
+          <?php print render($content['field_person_expertise']); ?>
+        <?php endif; ?>
+        <?php if ($content['body']): print render($content['body']); endif; ?>
+
     </div>
 
   <?php } ?>
-  <?php
-  drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.8/angular.min.js', array(
-          'type' => 'external',
-          'scope' => 'footer', ));
-  drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js', array(
-          'type' => 'external',
-          'scope' => 'footer', ));
-  drupal_add_js('https://apps.its.uiowa.edu/facadmin/assets/lib/angular-filter/dist/angular-filter.min.js?v=4', array(
-          'type' => 'external',
-          'scope' => 'footer', ));
-  drupal_add_js(drupal_get_path('theme','education_radix') . '/js/apr_profile.js', array('scope' => 'footer'));
-  ?>
 
 </article>
